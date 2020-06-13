6ebe29c9626e9d501b6986a2b5212cdcf819f223
Dong Zou <jiwenzou@gmail.com>
2020-06-13 15:57:03 -0700

    Finished-ish UserInterface
    - Work on Client and Server and figure out how to activate/connect them and how to transfer message from UI to Server/Client

--- src/_02_Chat_Application/UserInterface.java
+++ src/_02_Chat_Application/UserInterface.java
@@ -1,41 +1,142 @@
 package _02_Chat_Application;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.HierarchyBoundsListener;
+import java.awt.event.HierarchyEvent;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.awt.event.WindowStateListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-
-public class UserInterface {
+public class UserInterface implements ActionListener, FocusListener, HierarchyBoundsListener {
 
 	JFrame frame;
 	JPanel panel;
-	JTextField text;
+	JTextArea text;
+
+	JPanel textPanel;
+	JPanel messagePanel;
+
+	JButton sendButton;
+	public static int width = 500;
+	public static int height = 800;
+
 	
-
-	public static final int width = 500;
-	public static final int height = 800;
-
 	UserInterface() {
-		frame = new JFrame();
+		frame = new JFrame("Chat App");
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 		panel = new JPanel();
-			
+
 		frame.add(panel);
 		frame.setVisible(true);
-		frame.setSize(width, height);
+		frame.setPreferredSize(new Dimension(width, height));
+		panel.setPreferredSize(new Dimension(width, height));
 
-		text = new JTextField();
-		panel.add(text);
+		setPanels();
+
+		setTextBox();
+
+		frame.pack();
+		frame.addHierarchyBoundsListener(this);
+	}
+
+	void setPanels() {
+		textPanel = new JPanel();
+		messagePanel = new JPanel();
+
+		panel.add(messagePanel);
+		panel.add(textPanel);
+
+		messagePanel.setBackground(Color.blue);
+		messagePanel.setPreferredSize(new Dimension(width, height - height / 4));
+
+		textPanel.setBackground(Color.red);
+		textPanel.setPreferredSize(new Dimension(width, height / 10));
+
+	}
+
+	void setTextBox() {
+		text = new JTextArea();
+		textPanel.add(text);
+		sendButton = new JButton("Send");
+		textPanel.add(sendButton);
+
+		text.setPreferredSize(new Dimension(width - sendButton.getWidth() - 10, height / 20));
+		text.setText("Test");
+		text.addFocusListener(this);
+		text.setText("Send a Message");
 		
+		sendButton.setBackground(Color.blue);
+		sendButton.addActionListener(this);
 		
-		text.setSize(width, 200);
-		text.setPreferredSize(text.getSize());
-		text.setLocation(0, 100);
-		text.setText("Enter Text Here");
-	
 	}
 
 	public static void main(String[] args) {
 		UserInterface user = new UserInterface();
+		
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == sendButton) {
+			System.out.println(frame.getSize());
+		}
+		
+	}
+
+	boolean isEmpty = true;
+	@Override
+	public void focusGained(FocusEvent e) {
+		if(e.getSource() == text && isEmpty) {
+			text.setText(null);
+			System.out.println("gained");
+		}
+		
+	}
+
+	@Override
+	public void focusLost(FocusEvent e) {
+		if(e.getSource() == text && text.getText().isEmpty()) {
+			text.setText("Send a Message");
+			System.out.println("lost true");
+			isEmpty = true;
+		}else {
+			isEmpty = false;
+			System.out.println("lost false " + text.getText());
+		}
+		
+		
+		
+	}
+
+	@Override
+	public void ancestorMoved(HierarchyEvent e) {
+		if(e.getSource() == frame) {
+			System.out.println("frame moved");
+		}
+		
+	}
+
+	@Override
+	public void ancestorResized(HierarchyEvent e) {
+		if(e.getSource() == frame) {
+			System.out.println("frame resized");
+			
+		}
+		
+	}
+
+
+
 }

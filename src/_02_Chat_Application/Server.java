3fac21ba910a33774d3d997d2db29fcb7e22919a
Dong Zou <jiwenzou@gmail.com>
2020-07-06 20:08:12 -0700

    Chat App

--- src/_02_Chat_Application/Server.java
+++ src/_02_Chat_Application/Server.java
@@ -1,16 +1,82 @@
 package _02_Chat_Application;
 
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.UnknownHostException;
 
+import javax.swing.JOptionPane;
+
 public class Server {
-	
+	private int port;
+
+	private ServerSocket server;
+	private Socket connection;
+
+	ObjectOutputStream os;
+	ObjectInputStream is;
+
+	public Server(int port) {
+		this.port = port;
+	}
+
+	public void start(){
+		System.out.println("Server Started");
+		try {
+			server = new ServerSocket(port, 100);
+
+			connection = server.accept();
+
+			os = new ObjectOutputStream(connection.getOutputStream());
+			is = new ObjectInputStream(connection.getInputStream());
+
+			os.flush();
+
+			while (connection.isConnected()) {
+				
+				
+				try {
+					UserInterface.isConnected = true;
+					UserInterface.createMessage((String)is.readObject(), true);
+					System.out.println("(Server)Message Recieved as: " + is.readObject());
+				}catch(EOFException e) {
+					UserInterface.isConnected = false;
+					System.out.println("Connection Lost");
+					System.exit(0);
+					break;
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	public String getIPAddress() {
 		try {
 			return InetAddress.getLocalHost().getHostAddress();
 		} catch (UnknownHostException e) {
 			return "ERROR!!!!!";
+		}
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void sendMessage(String message) {
+		System.out.println("(Server)Message sent as: " + message);
+		try {
+			if (os != null) {
+				os.writeObject(message);
+				os.flush();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
--- src/_02_Chat_Application/UserInterface.java
+++ src/_02_Chat_Application/UserInterface.java
@@ -1,38 +1,39 @@
 package _02_Chat_Application;
 
 import java.awt.Color;
+import java.awt.ComponentOrientation;
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-import java.awt.event.HierarchyBoundsListener;
-import java.awt.event.HierarchyEvent;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-import java.awt.event.WindowStateListener;
+
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
 
-public class UserInterface implements ActionListener, FocusListener, HierarchyBoundsListener {
+public class UserInterface implements ActionListener, FocusListener {
 
 	JFrame frame;
 	JPanel panel;
-	JTextArea text;
 
-	JPanel textPanel;
-	JPanel messagePanel;
-
-	JButton sendButton;
+	public static boolean isConnected = false;
+	
 	public static int width = 500;
 	public static int height = 800;
 
+	public static String message = "";
 	
+	Server server;
+	Client client;
+	
+	boolean isEmpty = true;
 	UserInterface() {
 		frame = new JFrame("Chat App");
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
@@ -43,21 +44,55 @@ public class UserInterface implements ActionListener, FocusListener, HierarchyBo
 		frame.setPreferredSize(new Dimension(width, height));
 		panel.setPreferredSize(new Dimension(width, height));
 
-		setPanels();
-
-		setTextBox();
-
 		frame.pack();
-		frame.addHierarchyBoundsListener(this);
-	}
 
+		startPanel(true);
+	
+	}
+	
+	JPanel startPanel;
+	JPanel infoPanel;
+	JButton startServerButton;
+	JButton connectButton;
+	void startPanel(boolean visibility) {
+		startPanel = new JPanel();
+		infoPanel = new JPanel();
+		
+	//------------Start of infoPanel-----------------------------
+		
+		
+	//------------End of infoPanel------------------------------
+		
+	//------------Start of startPanel-----------------------------	 
+		connectButton.setVisible(true);
+		startServerButton.setVisible(true);
+		startServerButton = new JButton("Start Server");
+		startServerButton.addActionListener(this);
+		connectButton = new JButton("Connect");
+		connectButton.addActionListener(this);
+		
+		startPanel.add(startServerButton);
+		startPanel.add(connectButton);
+	
+	//------------End of startPanel-----------------------------
+		
+	}
+	
+	JPanel textPanel;
+	static JPanel messagePanel;
+	JPanel topPanel;
 	void setPanels() {
 		textPanel = new JPanel();
 		messagePanel = new JPanel();
-
+		topPanel = new JPanel();
+		
+		panel.add(topPanel);
 		panel.add(messagePanel);
 		panel.add(textPanel);
-
+		
+		topPanel.setBackground(Color.green);
+		topPanel.setPreferredSize(new Dimension(width, height / 10));
+		
 		messagePanel.setBackground(Color.blue);
 		messagePanel.setPreferredSize(new Dimension(width, height - height / 4));
 
@@ -65,7 +100,31 @@ public class UserInterface implements ActionListener, FocusListener, HierarchyBo
 		textPanel.setPreferredSize(new Dimension(width, height / 10));
 
 	}
+	
+	JButton exitButton;
+	JTextPane address;
+	void setTopPanel() {
+		address = new JTextPane();
+		
+		exitButton = new JButton();
+		exitButton.addActionListener(this);
+		
+		SimpleAttributeSet attribs = new SimpleAttributeSet();
+		StyleConstants.setAlignment(attribs, StyleConstants.ALIGN_CENTER);
+		address.setParagraphAttributes(attribs, true);
+		
+		topPanel.add(address);
+		address.setBackground(topPanel.getBackground());
+		address.setPreferredSize(new Dimension(width, (int)topPanel.getPreferredSize().getHeight() / 2));
+		
+		address.setText("Connected To IPAddress: " + server.getIPAddress() + "\nPort Number: " + server.getPort());
 
+		topPanel.add(exitButton);		
+
+	}
+	
+	JTextArea text;
+	JButton sendButton;
 	void setTextBox() {
 		text = new JTextArea();
 		textPanel.add(text);
@@ -81,26 +140,83 @@ public class UserInterface implements ActionListener, FocusListener, HierarchyBo
 		sendButton.addActionListener(this);
 		
 	}
-
+	
+	public static void createMessage(String str, boolean isRecieved) {
+		ArrayList<JTextArea> text = new ArrayList<JTextArea>();
+		text.add(new JTextArea());
+		
+		messagePanel.add(text.get(text.size() -1));
+		
+		if(isRecieved) {
+			text.get(text.size() -1).setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+			
+		}else {
+			text.get(text.size() -1).setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+			
+		}
+		
+		text.get(text.size() -1).setPreferredSize(new Dimension(width, messagePanel.getHeight() / 20));
+		text.get(text.size() -1).setText(str);
+		
+	}
+	
 	public static void main(String[] args) {
 		UserInterface user = new UserInterface();
-		
+	
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		
+	//--------------------Start of Send Button---------------------------
 		if(e.getSource() == sendButton) {
 			System.out.println(frame.getSize());
+			
 		}
+		
+		if(e.getSource() == sendButton) {
+			if(isEmpty == false) {
+			message = text.getText();
+			System.out.println("Set message equal to: " + message);
+			
+			createMessage(message, false);
+			server.sendMessage(message);
+			
+			isEmpty = true;
+			text.setText("Send a Message");
+			
+			}else{
+				
+				
+			}
+		}
+	//--------------------End of Send Button---------------------------
+		
+	//--------------------Start of Add/Server Button---------------------------
+		if(e.getSource() == connectButton) {
+			Thread clientThread = new Thread(() -> {
+				client.start();
+			});
+			
+		}
+		if(e.getSource() == startServerButton) {
+			Thread serverThread = new Thread(() -> {
+				server.start();
+			});
+			
+			
+			
+		}
+	//--------------------End of Add/Server Button-----------------------------
 		
 	}
 
-	boolean isEmpty = true;
+
 	@Override
 	public void focusGained(FocusEvent e) {
 		if(e.getSource() == text && isEmpty) {
 			text.setText(null);
-			System.out.println("gained");
+			System.out.println("Textbox focus gained");
 		}
 		
 	}
@@ -109,34 +225,11 @@ public class UserInterface implements ActionListener, FocusListener, HierarchyBo
 	public void focusLost(FocusEvent e) {
 		if(e.getSource() == text && text.getText().isEmpty()) {
 			text.setText("Send a Message");
-			System.out.println("lost true");
+			System.out.println("Focus lost with no message: " + text.getText());
 			isEmpty = true;
 		}else {
 			isEmpty = false;
-			System.out.println("lost false " + text.getText());
+			System.out.println("Focus lost with message: " + text.getText());
 		}
-		
-		
-		
 	}
-
-	@Override
-	public void ancestorMoved(HierarchyEvent e) {
-		if(e.getSource() == frame) {
-			System.out.println("frame moved");
-		}
-		
-	}
-
-	@Override
-	public void ancestorResized(HierarchyEvent e) {
-		if(e.getSource() == frame) {
-			System.out.println("frame resized");
-			
-		}
-		
-	}
-
-
-
 }
